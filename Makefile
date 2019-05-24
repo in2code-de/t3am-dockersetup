019ad8e035b8022a6e72d7b9664501cf23e10f7f
Stefan Busemann <stefan.busemann@in2code.de>
2019-05-24 14:21:36 +0200

    [WIP] Add support for additonal t3am instance based on TYPO3 v8

--- Makefile
+++ Makefile
@@ -12,7 +12,11 @@ logs:
 
 composer:
 	docker-compose exec client-php composer $(ARGS)
+	docker-compose exec client8-php composer $(ARGS)
 	docker-compose exec server-php composer $(ARGS)
+
+bash-client8:
+	docker-compose exec client8-php bash
 
 bash-client:
 	docker-compose exec client-php bash
@@ -22,10 +26,11 @@ bash-server:
 
 cert:
 	if [ ! -d ~/.dinghy/certs ]; then mkdir -p ~/.dinghy/certs; fi
-	cd ~/.dinghy/certs; bash -c 'openssl req -x509 -newkey rsa:2048 -keyout t3am.docker -out t3am.docker -days 365 -nodes -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=t3am.docker" -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:t3am.docker")) -reqexts SAN -extensions SAN'
+	cd ~/.dinghy/certs; bash -c 'openssl req -x509 -newkey rsa:2048 -keyout t3am.docker -out t3am.docker -out t3am8.docker-days 365 -nodes -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=t3am.docker" -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:t3am.docker")) -reqexts SAN -extensions SAN'
 
 init-var:
 	docker-compose exec -u0 client-php chown -R app:app /app/var
+	docker-compose exec -u0 client8-php chown -R app:app /app/var
 	docker-compose exec -u0 server-php chown -R app:app /app/var
 
 %:
--- client8/public/typo3conf/LocalConfiguration.php
+++ client8/public/typo3conf/LocalConfiguration.php
@@ -3,20 +3,16 @@ return [
     'BE' => [
         'debug' => false,
         'explicitADmode' => 'explicitAllow',
-        'installToolPassword' => '$argon2i$v=19$m=16384,t=16,p=2$N1M3SXBkQkV1TXZkUnpWWg$8InLSWGMCm+IFWd/eAQ/aN2qXt5QJ3PK7Knev0437Gc',
+        'installToolPassword' => md5('password'),
         'loginSecurityLevel' => 'normal',
-        'passwordHashing' => [
-            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
-            'options' => [],
-        ],
-    ],
+            ],
     'DB' => [
         'Connections' => [
             'Default' => [
                 'charset' => 'utf8mb4',
-                'dbname' => 'server',
+                'dbname' => 'client8',
                 'driver' => 'mysqli',
-                'host' => 'server-mysql',
+                'host' => 'client8-mysql',
                 'password' => 'dev',
                 'port' => 3306,
                 'tableoptions' => [
@@ -29,8 +25,9 @@ return [
     ],
     'EXT' => [
         'extConf' => [
-            'backend' => 'a:6:{s:9:"loginLogo";s:0:"";s:19:"loginHighlightColor";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}',
+            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
             'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
+            't3am' => 'a:4:{s:6:"server";s:14:"http://server/";s:5:"token";s:40:"f99c03a407da8eb841ef1aec428e21b8facfee62";s:12:"avatarFolder";s:10:"1:/avatar/";s:10:"selfSigned";s:1:"1";}',
         ],
     ],
     'EXTENSIONS' => [
@@ -45,6 +42,12 @@ return [
         'extensionmanager' => [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
+        ],
+        't3am' => [
+            'avatarFolder' => '1:/avatar/',
+            'selfSigned' => '1',
+            'server' => 'http://server/',
+            'token' => 'f99c03a407da8eb841ef1aec428e21b8facfee62',
         ],
     ],
     'FE' => [
@@ -75,16 +78,15 @@ return [
     'SYS' => [
         'devIPmask' => '',
         'displayErrors' => 0,
-        'encryptionKey' => 'aa95b606d16d6f3e39e94405d9ee9be8668c20235b81e5dc6bfea1244b8e640db3c4e02fb3c4eb0cdb46c54a79fb16ba',
+        'encryptionKey' => '52b030e57d93165edb70e2f7cb9e4bd7ba3f20fe937c17e99464b8be3fc0daec54f4e721cd2429ad646fea47c772626b',
         'exceptionalErrors' => 4096,
         'features' => [
             'unifiedPageTranslationHandling' => true,
         ],
-        'sitename' => 'T3AM SERVER',
+        'sitename' => 'T3AM Client',
         'systemLogLevel' => 2,
         'systemMaintainers' => [
             1,
-            3,
         ],
     ],
 ];
